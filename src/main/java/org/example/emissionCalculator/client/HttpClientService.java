204feaf070d106e5156d58cbeedf4d92743454a5
DinelyWellehewage <dinelywh78@gmail.com>
2025-01-14 11:46:51 +0100

    add HttpClientService to handle GET and POST requests

---
+++ src/main/java/org/example/emissionCalculator/client/HttpClientService.java
@@ -0,0 +1,61 @@
+package org.example.emissionCalculator.client;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class HttpClientService {
+
+    private final HttpClient httpClient;
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    public HttpClientService(){
+        this.httpClient = HttpClient.newHttpClient();
+    }
+
+    public <T> T sendGetRequest(String urlString,Class<T> responseType){
+        try {
+            URI uri = URI.create(urlString);
+            HttpRequest request = HttpRequest.newBuilder(uri)
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+            return handleResponse(response,responseType);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public <T> T sendPoseRequest(String urlString,String body,String apiKey,Class<T> responseType){
+        try {
+            URI uri = URI.create(urlString);
+            HttpRequest request = HttpRequest.newBuilder(uri)
+                    .header("Authorization",apiKey)
+                    .header("Content-type","application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(body))
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+            return handleResponse(response,responseType);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response,Class<T> responseType){
+
+        if (response.statusCode()==200){
+            try {
+                return objectMapper.readValue(response.body(),responseType);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }else {
+            throw new RuntimeException("HTTP request failed with status code: "+response.statusCode() + ",Body: "+response.body());
+        }
+    }
+}
