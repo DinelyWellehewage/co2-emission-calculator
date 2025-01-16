fd636e4aca0e154e1ec2b3a3f8f4b6463195be89
DinelyWellehewage <dinelywh78@gmail.com>
2025-01-16 19:25:48 +0100

    add comments to UserInteractionService

--- src/main/java/com/dev/emissionCalculator/service/UserInteractionService.java
+++ src/main/java/com/dev/emissionCalculator/service/UserInteractionService.java
@@ -5,10 +5,22 @@ import com.dev.emissionCalculator.model.response.LocationInfo;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * UserInteractionService is responsible for interacting with the user via the console
+ * It displays available coordinates and allows the user to select one by entering an index
+ */
 public class UserInteractionService {
 
+    /**
+     * Display a list of available coordinates along with their associated country and region
+     * and allows the user to select one
+     *
+     * @param coordinates a list of location information containing coordinates and metadata
+     * @return index of the selected coordinate from the list
+     */
+
     public int displayCityCoordinates(List<LocationInfo> coordinates) {
-        System.out.println("Available Coordinates: ");
+        System.out.println("Available Coordinates with Country and Region");
         for (int i = 0; i < coordinates.size(); i++) {
             String country = coordinates.get(i).getCountry();
             String region = coordinates.get(i).getRegion();
@@ -16,6 +28,13 @@ public class UserInteractionService {
         }
         return getUserSelectedIndex(coordinates.size());
     }
+
+    /**
+     * Prompts the user to select a valid index from the displayed list of coordinates
+     *
+     * @param maxIndex the maximum valid index for selection
+     * @return the valid index selected by the user
+     */
 
     private int getUserSelectedIndex(int maxIndex) {
         Scanner scanner = new Scanner(System.in);
